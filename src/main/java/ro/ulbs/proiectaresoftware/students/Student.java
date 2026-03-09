4b389a071a9f9079354d89c43aa12eefdac259ce
radu-cloud263 <raduioan.ispas@ulbsibiu.ro>
2026-03-09 19:48:52 +0200

    Student and Application classes

---
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
@@ -0,0 +1,38 @@
+package ro.ulbs.proiectaresoftware.students;
+
+public class Student {
+    int numarMatricol;
+    String Prenume;
+    String Nume;
+
+    public int getNumarMatricol() {
+        return numarMatricol;
+    }
+
+    public String getPrenume() {
+        return Prenume;
+    }
+
+    public String getNume() {
+        return Nume;
+    }
+
+    public String getFormatieDeStudiu() {
+        return formatieDeStudiu;
+    }
+
+    @Override
+    public String toString() {
+        // Folosim String.format pentru a alinia textul pe coloane
+        return String.format("%10d %14s %11s %15s", numarMatricol, Prenume, Nume, formatieDeStudiu);
+    }
+
+    String formatieDeStudiu;
+
+    public Student(int numarMatricol, String prenume, String nume, String formatieDeStudiu) {
+        this.numarMatricol = numarMatricol;
+        Prenume = prenume;
+        Nume = nume;
+        this.formatieDeStudiu = formatieDeStudiu;
+    }
+}
