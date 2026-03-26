2400c9ab00d04b9dbcebe7325e4787253d4d32c3
radu-cloud263 <raduioan.ispas@ulbsibiu.ro>
2026-03-26 22:00:13 +0200

    implementare fisier "note_anon.txt" si HashMap

--- src/main/java/ro/ulbs/proiectaresoftware/students/Main.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Main.java
@@ -1,65 +1,81 @@
 package ro.ulbs.proiectaresoftware.students;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.io.File;
-import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.io.PrintWriter;
 import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Student> listaStudenti = new ArrayList<>();
 
-        try {
-            File fisier = new File("studenti_in.txt");
-            Scanner scanner = new Scanner(fisier);
 
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
+            Map<Integer, Student> mapStudenti = new HashMap<>();
+            List<Student> listaStudenti = new ArrayList<>();
 
-                String[] date = line.split(",");
 
-                if (date.length == 4) {
-                    int numarMatricol = Integer.parseInt(date[0].trim());
-                    String prenume = date[1].trim();
-                    String nume = date[2].trim();
-                    String formatieDeStudiu = date[3].trim();
+            try (Scanner scanner = new Scanner(new File("studenti_in.txt"))) {
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    String[] date = line.split(",");
 
-                    Student student = new Student(numarMatricol, prenume, nume, formatieDeStudiu);
-                    listaStudenti.add(student);
+                    if (date.length == 4) {
+                        int numarMatricol = Integer.parseInt(date[0].trim());
+                        String prenume = date[1].trim();
+                        String nume = date[2].trim();
+                        String formatieDeStudiu = date[3].trim();
+
+                        Student student = new Student(numarMatricol, prenume, nume, formatieDeStudiu);
+
+
+                        mapStudenti.put(numarMatricol, student);
+                        listaStudenti.add(student);
+                    }
                 }
-
-            }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            System.err.println("File not found!");
-            return;
-        }
-
-        System.out.println("Studenti nesortati:");
-        for (Student s : listaStudenti) {
-            System.out.println(s.toString());
-        }
-
-        listaStudenti.sort(
-                Comparator.comparing(Student::getFormatieDeStudiu).thenComparing(Student::getNume)
-        );
-
-        try {
-            File fisierOutSorted = new File("studenti_out_sorted.txt");
-            PrintWriter writer = new PrintWriter(fisierOutSorted);
-
-            for (Student s : listaStudenti) {
-                writer.println(s.getNumarMatricol() + "," + s.getPrenume() + "," + s.getNume() + "," + s.getFormatieDeStudiu());
+            } catch (FileNotFoundException e) {
+                System.err.println("Eroare: Fisierul 'studenti_in.txt' nu a fost gasit");
+                return;
             }
 
-            writer.close();
-            System.out.println("\nStudentii au fost sortati si salvati in 'studenti_out_sorted.txt'");
-        } catch (FileNotFoundException e) {
-            System.err.println("Eroare: Nu s-a putut accesa fisierul 'studenti_out_sorted.txt'");
-        }
+
+            try (Scanner scannerNote = new Scanner(new File("note_anon.txt"))) {
+                while (scannerNote.hasNextLine()) {
+                    String line = scannerNote.nextLine();
+                    String[] date = line.split(",");
+
+                    if (date.length >= 2) {
+                        int numarMatricol = Integer.parseInt(date[0].trim());
+                        double nota = Double.parseDouble(date[1].trim());
+
+
+                        Student studentGasit = mapStudenti.get(numarMatricol);
+
+
+                        if (studentGasit != null) {
+                            studentGasit.setNota(nota);
+                        }
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                System.err.println("Eroare: fisierul 'note_anon.txt' nu a fost gasit");
+            }
+
+            System.out.println("Studenti si Note din Map:");
+            for (Map.Entry<Integer, Student> entry : mapStudenti.entrySet()) {
+                System.out.println(entry.getValue().toString());
+            }
+
+            listaStudenti.sort(
+                    Comparator.comparing(Student::getFormatieDeStudiu).thenComparing(Student::getNume)
+            );
+
+            try (PrintWriter writer = new PrintWriter(new File("studenti_out_sorted.txt"))) {
+                for (Student s : listaStudenti) {
+                    writer.println(s.getNumarMatricol() + "," + s.getPrenume() + "," + s.getNume() + "," + s.getFormatieDeStudiu() + "," + s.getNota());
+                }
+                System.out.println("Studentii au fost sortati si salvati in 'studenti_out_sorted.txt'");
+            } catch (FileNotFoundException e) {
+                System.err.println("Eroare: Nu s-a putut crea/accesa fisierul 'studenti_out_sorted.txt'");
+            }
     }
-
 }
--- src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
@@ -2,12 +2,15 @@ package ro.ulbs.proiectaresoftware.students;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Student {
     int numarMatricol;
     String Prenume;
     String Nume;
+    String formatieDeStudiu;
+    double Nota;
 
     public int getNumarMatricol() {
         return numarMatricol;
@@ -25,18 +28,33 @@ public class Student {
         return formatieDeStudiu;
     }
 
+    public double getNota() { return Nota; }
+
     @Override
-    public String toString() {
-        return String.format("%10d %14s %11s %15s", numarMatricol, Prenume, Nume, formatieDeStudiu);
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return numarMatricol == student.numarMatricol;
     }
 
-    String formatieDeStudiu;
+    @Override
+    public int hashCode() {
+        return Objects.hash(numarMatricol, Prenume, Nume, formatieDeStudiu, Nota);
+    }
+
+    public void setNota(double nota) { this.Nota = nota; };
+
+    @Override
+    public String toString() {
+        return String.format("%10d %14s %11s %15s %5.2f", numarMatricol, Prenume, Nume, formatieDeStudiu, Nota);
+    }
 
     public Student(int numarMatricol, String prenume, String nume, String formatieDeStudiu) {
         this.numarMatricol = numarMatricol;
         Prenume = prenume;
         Nume = nume;
         this.formatieDeStudiu = formatieDeStudiu;
+        this.Nota = 0;
     }
 }
 
