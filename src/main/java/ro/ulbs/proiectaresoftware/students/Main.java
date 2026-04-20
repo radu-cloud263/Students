053e1a797b9d0630d4935b7b151208249ad9c2f4
radu-cloud263 <raduioan.ispas@ulbsibiu.ro>
2026-04-20 21:40:02 +0300

    implementare clasa StudentBursier

--- src/main/java/ro/ulbs/proiectaresoftware/students/Main.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Main.java
@@ -5,77 +5,36 @@ import java.io.File;
 import java.util.ArrayList;
 import java.io.PrintWriter;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+
 
 public class Main {
+
+
+    public static void salveazaInFisier(String numeFisier, Collection<? extends Student> colectieStudenti) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(numeFisier))) {
+            for (Student student : colectieStudenti) {
+                writer.write(student.toString());
+                writer.newLine();
+            }
+            System.out.println("Lista a fost salvata cu succes in: " + numeFisier);
+        } catch (IOException e) {
+            System.err.println("Eroare la scrierea in fisierul " + numeFisier + ": " + e.getMessage());
+        }
+    }
     public static void main(String[] args) {
 
 
-            Map<Integer, Student> mapStudenti = new HashMap<>();
-            List<Student> listaStudenti = new ArrayList<>();
+        List<StudentBursier> bursieri = new ArrayList<>();
+
+        bursieri.add(new StudentBursier(1025, "Andrei", "Popa", "ISM141/2", 8.70, 725.50));
+        bursieri.add(new StudentBursier(1024, "Ioan", "Mihalcea", "ISM141/1", 9.80, 801.10));
+        bursieri.add(new StudentBursier(1026, "Anamaria", "Prodan", "TI131/1", 8.90, 745.50));
+        bursieri.add(new StudentBursier(1029, "Bianca", "Popescu", "TI131/1", 9.10, 780.80));
 
 
-            try (Scanner scanner = new Scanner(new File("studenti_in.txt"))) {
-                while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine();
-                    String[] date = line.split(",");
-
-                    if (date.length == 4) {
-                        int numarMatricol = Integer.parseInt(date[0].trim());
-                        String prenume = date[1].trim();
-                        String nume = date[2].trim();
-                        String formatieDeStudiu = date[3].trim();
-
-                        Student student = new Student(numarMatricol, prenume, nume, formatieDeStudiu);
-
-
-                        mapStudenti.put(numarMatricol, student);
-                        listaStudenti.add(student);
-                    }
-                }
-            } catch (FileNotFoundException e) {
-                System.err.println("Eroare: Fisierul 'studenti_in.txt' nu a fost gasit");
-                return;
-            }
-
-
-            try (Scanner scannerNote = new Scanner(new File("note_anon.txt"))) {
-                while (scannerNote.hasNextLine()) {
-                    String line = scannerNote.nextLine();
-                    String[] date = line.split(",");
-
-                    if (date.length >= 2) {
-                        int numarMatricol = Integer.parseInt(date[0].trim());
-                        double nota = Double.parseDouble(date[1].trim());
-
-
-                        Student studentGasit = mapStudenti.get(numarMatricol);
-
-
-                        if (studentGasit != null) {
-                            studentGasit.setNota(nota);
-                        }
-                    }
-                }
-            } catch (FileNotFoundException e) {
-                System.err.println("Eroare: fisierul 'note_anon.txt' nu a fost gasit");
-            }
-
-            System.out.println("Studenti si Note din Map:");
-            for (Map.Entry<Integer, Student> entry : mapStudenti.entrySet()) {
-                System.out.println(entry.getValue().toString());
-            }
-
-            listaStudenti.sort(
-                    Comparator.comparing(Student::getFormatieDeStudiu).thenComparing(Student::getNume)
-            );
-
-            try (PrintWriter writer = new PrintWriter(new File("studenti_out_sorted.txt"))) {
-                for (Student s : listaStudenti) {
-                    writer.println(s.getNumarMatricol() + "," + s.getPrenume() + "," + s.getNume() + "," + s.getFormatieDeStudiu() + "," + s.getNota());
-                }
-                System.out.println("Studentii au fost sortati si salvati in 'studenti_out_sorted.txt'");
-            } catch (FileNotFoundException e) {
-                System.err.println("Eroare: Nu s-a putut crea/accesa fisierul 'studenti_out_sorted.txt'");
-            }
+        salveazaInFisier("bursieri_out.txt", bursieri);
     }
 }
---
+++ src/main/java/ro/ulbs/proiectaresoftware/students/StudentBursier.java
@@ -0,0 +1,42 @@
+package ro.ulbs.proiectaresoftware.students;
+
+import java.util.Objects;
+
+public class StudentBursier extends Student {
+    private double cuantumBursa;
+
+    public StudentBursier(int numarMatricol, String prenume, String nume, String formatieDeStudiu, double nota, double cuantumBursa) {
+
+        super(numarMatricol, prenume, nume, formatieDeStudiu);
+
+        this.setNota(nota);
+        this.cuantumBursa = cuantumBursa;
+    }
+
+    public double getCuantumBursa() {
+        return cuantumBursa;
+    }
+
+    public void setCuantumBursa(double cuantumBursa) {
+        this.cuantumBursa = cuantumBursa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        StudentBursier that = (StudentBursier) o;
+        return Double.compare(that.cuantumBursa, cuantumBursa) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), cuantumBursa);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(" %8.2f", cuantumBursa);
+    }
+}
